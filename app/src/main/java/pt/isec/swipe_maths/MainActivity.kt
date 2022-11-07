13b252abd1a80e0177ea9cb4fdbc539af0c7eaa3
Pedro Correia <pedrogrcorreia@gmail.com>
2022-11-07 12:25:26 +0000

    main activity with buttons just making a snackbar

--- app/src/main/java/pt/isec/swipe_maths/MainActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/MainActivity.kt
@@ -2,10 +2,25 @@ package pt.isec.swipe_maths
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonSingle : Button = findViewById(R.id.singlePlayer)
+        var buttonMulti : Button = findViewById(R.id.multiPlayer)
+        var buttonProfile : Button = findViewById(R.id.userProfile)
+
+        buttonSingle.setOnClickListener(makeSnackbar)
+        buttonMulti.setOnClickListener(makeSnackbar)
+        buttonProfile.setOnClickListener(makeSnackbar)
+    }
+
+    private val makeSnackbar = View.OnClickListener {
+        Snackbar.make(it, "${(it as Button).text}: ${getString(R.string.todo)}", Snackbar.LENGTH_LONG).show()
     }
 }
