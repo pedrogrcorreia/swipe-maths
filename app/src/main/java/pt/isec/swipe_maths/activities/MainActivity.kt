dffe6ed4882133581abdf5be53a89c61ce3634e7
Pedro Correia <pedrogrcorreia@gmail.com>
2022-11-23 21:51:57 +0000

    add binding on main activity. add buttons to sign in and update ui with current user state

--- app/src/main/java/pt/isec/swipe_maths/activities/MainActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/activities/MainActivity.kt
@@ -11,25 +11,36 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import pt.isec.swipe_maths.R
+import pt.isec.swipe_maths.databinding.ActivityMainBinding
 import pt.isec.swipe_maths.model.board.Board
 import pt.isec.swipe_maths.model.board.Column
 import pt.isec.swipe_maths.model.board.Line
 import pt.isec.swipe_maths.model.levels.Levels
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonSingle : Button = findViewById(R.id.singlePlayer)
-        val buttonMulti : Button = findViewById(R.id.multiPlayer)
-        val buttonProfile : Button = findViewById(R.id.userProfile)
+        auth = Firebase.auth
 
-        buttonSingle.setOnClickListener {
+        updateUI()
+
+        binding.singlePlayer.setOnClickListener {
             startActivity(GameScreenActivity.getSingleModeIntent(this))
         }
-        buttonMulti.setOnClickListener{
+
+        binding.multiPlayer.setOnClickListener{
             val dlg = AlertDialog.Builder(this)
                 .setTitle("Multiplayer")
                 .setMessage("Want to be server or client")
@@ -43,10 +54,33 @@ class MainActivity : AppCompatActivity() {
 
             dlg.show()
         }
-        buttonProfile.setOnClickListener(makeSnackbar)
+
+        binding.userProfile.setOnClickListener{
+            if(auth.currentUser == null){
+                AlertDialog.Builder(this)
+                    .setTitle("Login")
+                    .setMessage("You are not logged in")
+                    .show()
+            }
+        }
+
+
     }
 
     private val makeSnackbar = View.OnClickListener {
         Snackbar.make(it, "${(it as Button).text}: ${getString(R.string.todo)}", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun updateUI(){
+        if(auth.currentUser != null){
+            binding.emailButton.visibility = View.INVISIBLE
+            binding.googleButton.visibility = View.INVISIBLE
+            binding.welcomeTxt.visibility = View.VISIBLE
+        } else {
+            binding.emailButton.visibility = View.VISIBLE
+            binding.googleButton.visibility = View.VISIBLE
+            binding.welcomeTxt.visibility = View.INVISIBLE
+            binding.welcomeTxt.text = getString(R.string.welcome, auth.currentUser!!.displayName)
+        }
     }
 }
