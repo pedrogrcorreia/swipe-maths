4a3322148fc4d5435075cf2c6d25bf818ee979c5
Pedro Correia <pedrogrcorreia@gmail.com>
2022-11-23 22:25:14 +0000

    sign-in with google and email working

--- app/src/main/java/pt/isec/swipe_maths/activities/MainActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/activities/MainActivity.kt
@@ -1,25 +1,24 @@
 package pt.isec.swipe_maths.activities
 
 import android.content.DialogInterface
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.util.Patterns
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import pt.isec.swipe_maths.R
 import pt.isec.swipe_maths.databinding.ActivityMainBinding
-import pt.isec.swipe_maths.model.board.Board
-import pt.isec.swipe_maths.model.board.Column
-import pt.isec.swipe_maths.model.board.Line
-import pt.isec.swipe_maths.model.levels.Levels
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,12 +26,22 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
 
+    private lateinit var googleSignInClient : GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = Firebase.auth
+
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         updateUI()
 
@@ -64,11 +73,41 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        binding.emailButton.setOnClickListener {
+            auth.signInWithEmailAndPassword("pedrogrcorreia@gmail.com", "123456").addOnSuccessListener {
+                updateUI()
+            }
+        }
 
+        binding.googleButton.setOnClickListener {
+            signInWithGoogle.launch(googleSignInClient.signInIntent)
+        }
+    }
+
+    val signInWithGoogle = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+        try{
+            val account = task.getResult(ApiException::class.java)!!
+            println("firebaseAuthWithGoogle: ${account.id}")
+            firebaseAuthWithGoogle(account.idToken!!)
+        } catch (e: ApiException){
+            println("Login with google failed! ${e.message}")
+        }
     }
 
     private val makeSnackbar = View.OnClickListener {
         Snackbar.make(it, "${(it as Button).text}: ${getString(R.string.todo)}", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnSuccessListener(this) { result ->
+                println("Success login google!")
+                updateUI()
+            }
     }
 
     private fun updateUI(){
@@ -76,11 +115,11 @@ class MainActivity : AppCompatActivity() {
             binding.emailButton.visibility = View.INVISIBLE
             binding.googleButton.visibility = View.INVISIBLE
             binding.welcomeTxt.visibility = View.VISIBLE
+            binding.welcomeTxt.text = getString(R.string.welcome, auth.currentUser!!.displayName)
         } else {
             binding.emailButton.visibility = View.VISIBLE
             binding.googleButton.visibility = View.VISIBLE
             binding.welcomeTxt.visibility = View.INVISIBLE
-            binding.welcomeTxt.text = getString(R.string.welcome, auth.currentUser!!.displayName)
         }
     }
 }
