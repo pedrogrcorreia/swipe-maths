19b653db60c1f8fc28ded9f6a63b10697b436154
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-05 19:36:00 +0000

    add fragments related to multiplayer activity

--- app/src/main/java/pt/isec/swipe_maths/activities/MultiplayerActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/activities/MultiplayerActivity.kt
@@ -3,10 +3,21 @@ package pt.isec.swipe_maths.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import pt.isec.swipe_maths.R
+import pt.isec.swipe_maths.databinding.ActivityMultiplayerBinding
+import pt.isec.swipe_maths.fragments.ClientFragment
 
 class MultiplayerActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMultiplayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_multiplayer)
+        binding = ActivityMultiplayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClient.setOnClickListener {
+            var mainFragment: ClientFragment = ClientFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.mmLayout, mainFragment).commit()
+        }
     }
 }
