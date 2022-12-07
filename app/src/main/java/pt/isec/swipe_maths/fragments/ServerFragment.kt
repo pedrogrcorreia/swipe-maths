63b3804bb9a1f51658fe81b2d9e5e4e31c3edab3
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-07 23:57:40 +0000

    add recyclerview list with to show connected players

--- app/src/main/java/pt/isec/swipe_maths/fragments/ServerFragment.kt
+++ app/src/main/java/pt/isec/swipe_maths/fragments/ServerFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import pt.isec.swipe_maths.R
 import pt.isec.swipe_maths.databinding.FragmentServerBinding
+import pt.isec.swipe_maths.model.Player
+import java.net.URL
 
 class ServerFragment : Fragment() {
 
@@ -19,12 +25,48 @@ class ServerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentServerBinding.inflate(inflater)
+        binding = FragmentServerBinding.inflate(inflater, container, false)
 
+        var playersList = binding.rvList
+
+        playersList.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        val players = arrayListOf(Player("Pedro", URL("https://google.com")), Player("José", URL("https://google.com")))
+
+        playersList.adapter = PlayerListAdapter(players)
         return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    class PlayerListAdapter(val data: ArrayList<Player>) : RecyclerView.Adapter<PlayerListAdapter.MyViewHolder>(){
+        class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view){
+            var name : TextView = view.findViewById(R.id.playerName)
+            var photo : ImageView = view.findViewById(R.id.playerPhoto)
+
+            fun update(data : Player){
+                name.text = data.name
+                // put photo!
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.connected_player_list, parent, false)
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.update(data[position])
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
     }
 }
---
+++ app/src/main/java/pt/isec/swipe_maths/model/Player.kt
@@ -0,0 +1,5 @@
+package pt.isec.swipe_maths.model
+
+import java.net.URL
+
+data class Player(val name: String, val photoUrl: URL)
