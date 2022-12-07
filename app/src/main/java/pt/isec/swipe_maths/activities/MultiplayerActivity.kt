ed7f1ebddb7ad82fedb38fa7594169b19b73349c
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-07 19:31:06 +0000

    start/cancel fragments on multiplayer activity

--- app/src/main/java/pt/isec/swipe_maths/activities/MultiplayerActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/activities/MultiplayerActivity.kt
@@ -1,10 +1,14 @@
 package pt.isec.swipe_maths.activities
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import pt.isec.swipe_maths.R
 import pt.isec.swipe_maths.databinding.ActivityMultiplayerBinding
 import pt.isec.swipe_maths.fragments.ClientFragment
+import pt.isec.swipe_maths.fragments.ServerFragment
 
 class MultiplayerActivity : AppCompatActivity() {
 
@@ -15,9 +19,31 @@ class MultiplayerActivity : AppCompatActivity() {
         binding = ActivityMultiplayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var mainFragment : Fragment = Fragment()
+
+        binding.btnCancel.isEnabled = false
+
         binding.btnClient.setOnClickListener {
-            var mainFragment: ClientFragment = ClientFragment()
-            supportFragmentManager.beginTransaction().replace(R.id.mmLayout, mainFragment).commit()
+            mainFragment = ClientFragment()
+            supportFragmentManager.beginTransaction().add(R.id.mmLayout, mainFragment).commit()
+            binding.btnClient.isEnabled = false
+            binding.btnServer.isEnabled = false
+            binding.btnCancel.isEnabled = true
+        }
+
+        binding.btnServer.setOnClickListener {
+            mainFragment = ServerFragment()
+            supportFragmentManager.beginTransaction().add(R.id.mmLayout, mainFragment).commit()
+            binding.btnClient.isEnabled = false
+            binding.btnServer.isEnabled = false
+            binding.btnCancel.isEnabled = true
+        }
+
+        binding.btnCancel.setOnClickListener{
+            supportFragmentManager.beginTransaction().remove(mainFragment).commit()
+            binding.btnClient.isEnabled = true
+            binding.btnServer.isEnabled = true
+            binding.btnCancel.isEnabled = false
         }
     }
 }
--- app/src/main/java/pt/isec/swipe_maths/fragments/ServerFragment.kt
+++ app/src/main/java/pt/isec/swipe_maths/fragments/ServerFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import pt.isec.swipe_maths.R
+import pt.isec.swipe_maths.databinding.FragmentServerBinding
 
 class ServerFragment : Fragment() {
 
+    lateinit var binding : FragmentServerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,8 +19,12 @@ class ServerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_server, container, false)
+        binding = FragmentServerBinding.inflate(inflater)
+
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
