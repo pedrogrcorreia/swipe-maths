69808a10be81d58d6b5b627ddeddfd19931f7f61
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-08 00:34:09 +0000

    search for server on client fragment

--- app/src/main/java/pt/isec/swipe_maths/fragments/ClientFragment.kt
+++ app/src/main/java/pt/isec/swipe_maths/fragments/ClientFragment.kt
@@ -5,9 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import pt.isec.swipe_maths.R
+import pt.isec.swipe_maths.databinding.FragmentClientBinding
+import pt.isec.swipe_maths.utils.NetUtils
+import kotlin.concurrent.thread
 
 class ClientFragment : Fragment() {
+
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Unconfined)
+
+    lateinit var binding : FragmentClientBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +30,39 @@ class ClientFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client, container, false)
+        binding = FragmentClientBinding.inflate(inflater, container, false)
+
+        binding.btnSearch.setOnClickListener {
+            thread {
+                scope.launch {
+                    val job = launch {
+                        val ipAddress = NetUtils.contactMulticast()
+                        if(ipAddress != null){
+                            activity?.runOnUiThread {
+                                Toast.makeText(context, getString(R.string.error_timeout), Toast.LENGTH_LONG)
+                            }
+                        }
+                    }
+                    if(job.isActive){
+                        activity?.runOnUiThread {
+                            loadingDialog.show()
+                        }
+                    }
+                }.invokeOnCompletion {
+                    loadingDialog.dismiss()
+                }
+            }
+        }
+
+
+        return binding.root
+    }
+
+    private val loadingDialog: AlertDialog by lazy {
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.loading_title))
+            .setMessage(getString(R.string.loading_message))
+            .setView(ProgressBar(requireContext()))
+            .create()
     }
 }
