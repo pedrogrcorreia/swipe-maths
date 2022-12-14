3abf6e24576d8284393331cc88bb82618663d9aa
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-14 01:27:39 +0000

    add line serialization

--- app/src/main/java/pt/isec/swipe_maths/model/board/Line.kt
+++ app/src/main/java/pt/isec/swipe_maths/model/board/Line.kt
@@ -1,9 +1,11 @@
 package pt.isec.swipe_maths.model.board
 
+import com.google.gson.*
 import pt.isec.swipe_maths.model.levels.Levels
+import java.lang.reflect.Type
 import kotlin.random.Random
 
-class Line(level: Levels = Levels.Easy) {
+class Line(level: Levels = Levels.Easy) : JsonSerializer<Line>, JsonDeserializer<Line> {
     companion object{
         private val random : Random = Random(System.currentTimeMillis())
     }
@@ -12,7 +14,6 @@ class Line(level: Levels = Levels.Easy) {
     val lineValue : Int
 
     init{
-        println("$random")
         numbers = arrayOf(random.nextInt(level.min, level.max),
             random.nextInt(level.min, level.max),
             random.nextInt(level.min, level.max))
@@ -20,6 +21,11 @@ class Line(level: Levels = Levels.Easy) {
             level.validOperations[random.nextInt(level.validOperations.size)]
             )
         lineValue = lineValue()
+    }
+
+    constructor(numbers: Array<Int>, operators: Array<String>) : this(){
+        this.numbers = numbers
+        this.operators = operators
     }
 
     fun printLine(): String {
@@ -33,5 +39,47 @@ class Line(level: Levels = Levels.Easy) {
 //            result = Operations.calculate(result, operators[i], numbers[i+1])
 //        }
         return Operations.calculateExpression(numbers, operators)
+    }
+
+    override fun serialize(
+        src: Line?,
+        typeOfSrc: Type?,
+        context: JsonSerializationContext?
+    ): JsonObject {
+        val jsonObject = JsonObject()
+
+        try {
+
+            jsonObject.add("numbers", context?.serialize(src?.numbers))
+            jsonObject.add("operators", context?.serialize(src?.operators))
+
+        }catch (e: Exception){
+            println(e.message)
+        }
+
+        return jsonObject
+    }
+
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext?
+    ): Line? {
+        val json = json.asJsonObject
+
+        val n = json.getAsJsonArray("numbers")
+        val numbers : Array<Int> = Array(n.size()) { 0 }
+
+        for(i in 0 until n.size()){
+            numbers[i] = n.get(i).asInt
+        }
+
+        val o = json.getAsJsonArray("operators")
+        val operators : Array<String> = Array(o.size()) { "" }
+
+        for(i in 0 until o.size()){
+            operators[i] = o.get(i).asString
+        }
+        return Line(numbers, operators)
     }
 }
