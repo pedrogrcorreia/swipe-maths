0b74c72e68ca0a2f9e26145c3950d76388abef92
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-14 03:31:09 +0000

    add test tools

--- app/src/test/java/pt/isec/swipe_maths/ExampleUnitTest.kt
+++ app/src/test/java/pt/isec/swipe_maths/ExampleUnitTest.kt
@@ -1,9 +1,17 @@
 package pt.isec.swipe_maths
 
-import org.junit.Test
-
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import org.junit.Assert.*
-import pt.isec.swipe_maths.utils.FirestoreUtils
+import org.junit.Rule
+import org.junit.Test
+import pt.isec.swipe_maths.model.Game
+import pt.isec.swipe_maths.model.GameSerializer
+import pt.isec.swipe_maths.model.board.Board
+import pt.isec.swipe_maths.model.board.Column
+import pt.isec.swipe_maths.model.board.Line
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,13 +19,80 @@ import pt.isec.swipe_maths.utils.FirestoreUtils
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
 
     @Test
-    fun getData(){
-        FirestoreUtils.highscoresSinglePlayer()
+    fun testGame(){
+        val game = Game()
+
+        val gson = GsonBuilder()
+//            .registerTypeAdapter(Game::class.java, GameSerializer())
+            .create()
+
+        val json = gson.toJson(game, Game::class.java)
+
+
+        val gameFromJson = gson.fromJson(json, Game::class.java)
+//        gameFromJson.boards()
+        println(json)
+        println("game board " + game.board.value?.printBoard())
+        println("json game board " + gameFromJson.board.value?.printBoard())
+        println("game level " + game.level.value)
+        println("json game level " + gameFromJson.level.value )
+        println("game board " + game.boardData?.printBoard())
+        println("json game board " + gameFromJson.boardData?.printBoard())
+    }
+
+    @Test
+    fun testBoard(){
+        val board = Board()
+
+        val gson = GsonBuilder()
+            .create()
+
+        val json = gson.toJson(board, Board::class.java)
+
+        val boardFromJson = gson.fromJson(json, Board::class.java)
+
+        println(json)
+        println("Board " + board.printBoard())
+        println("Board Json: " + boardFromJson.printBoard())
+    }
+
+    @Test
+    fun testLine(){
+        val line = Line()
+
+        val gson = GsonBuilder()
+            .create()
+
+        val json = gson.toJson(line, Line::class.java)
+
+
+        val lineFromJson = gson.fromJson(json, Line::class.java)
+        println(json)
+        println("line: " + line.printLine())
+        println("json line: ${lineFromJson.printLine()}")
+    }
+
+    @Test
+    fun testCol(){
+        val col = Column(1, 2, 3)
+
+        val gson = Gson()
+
+        val json = gson.toJson(col, Column::class.java)
+
+        val colFromJson = gson.fromJson(json, Column::class.java)
+
+        println(json)
+        println("col: " + col.printColumn())
+        println("json col: " + colFromJson.printColumn())
     }
 }
