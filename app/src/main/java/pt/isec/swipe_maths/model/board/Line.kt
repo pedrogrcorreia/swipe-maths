bfa195066970653acdb7c8503b6af9d3d950edf0
Pedro Correia <pedrogrcorreia@gmail.com>
2022-12-14 03:30:57 +0000

    removed serialization implement

--- app/src/main/java/pt/isec/swipe_maths/model/board/Line.kt
+++ app/src/main/java/pt/isec/swipe_maths/model/board/Line.kt
@@ -5,7 +5,7 @@ import pt.isec.swipe_maths.model.levels.Levels
 import java.lang.reflect.Type
 import kotlin.random.Random
 
-class Line(level: Levels = Levels.Easy) : JsonSerializer<Line>, JsonDeserializer<Line> {
+class Line(level: Levels = Levels.Easy) {
     companion object{
         private val random : Random = Random(System.currentTimeMillis())
     }
@@ -23,63 +23,12 @@ class Line(level: Levels = Levels.Easy) : JsonSerializer<Line>, JsonDeserializer
         lineValue = lineValue()
     }
 
-    constructor(numbers: Array<Int>, operators: Array<String>) : this(){
-        this.numbers = numbers
-        this.operators = operators
-    }
-
     fun printLine(): String {
         return "${numbers[0]} ${operators[0]} " +
                 "${numbers[1]} ${operators[1]} ${numbers[2]} = $lineValue"
     }
 
     private fun lineValue() : Int {
-//        var result = numbers[0]
-//        for(i in operators.indices){
-//            result = Operations.calculate(result, operators[i], numbers[i+1])
-//        }
         return Operations.calculateExpression(numbers, operators)
-    }
-
-    override fun serialize(
-        src: Line?,
-        typeOfSrc: Type?,
-        context: JsonSerializationContext?
-    ): JsonObject {
-        val jsonObject = JsonObject()
-
-        try {
-
-            jsonObject.add("numbers", context?.serialize(src?.numbers))
-            jsonObject.add("operators", context?.serialize(src?.operators))
-
-        }catch (e: Exception){
-            println(e.message)
-        }
-
-        return jsonObject
-    }
-
-    override fun deserialize(
-        json: JsonElement,
-        typeOfT: Type,
-        context: JsonDeserializationContext?
-    ): Line? {
-        val json = json.asJsonObject
-
-        val n = json.getAsJsonArray("numbers")
-        val numbers : Array<Int> = Array(n.size()) { 0 }
-
-        for(i in 0 until n.size()){
-            numbers[i] = n.get(i).asInt
-        }
-
-        val o = json.getAsJsonArray("operators")
-        val operators : Array<String> = Array(o.size()) { "" }
-
-        for(i in 0 until o.size()){
-            operators[i] = o.get(i).asString
-        }
-        return Line(numbers, operators)
     }
 }
