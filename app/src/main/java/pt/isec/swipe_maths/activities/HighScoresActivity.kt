e6821b8bf4b5437da61a0663f4d8d1972e11ec7d
Pedro Correia <pedrogrcorreia@gmail.com>
2023-01-02 18:30:59 +0000

    change tab layout titles to string resources

--- app/src/main/java/pt/isec/swipe_maths/activities/HighScoresActivity.kt
+++ app/src/main/java/pt/isec/swipe_maths/activities/HighScoresActivity.kt
@@ -27,10 +27,6 @@ class HighScoresActivity : AppCompatActivity() {
         }
     }
 
-    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Unconfined)
-
-    lateinit var highscores : ArrayList<SinglePlayerGame>
-
     lateinit var binding : ActivityHighScoresBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,76 +43,9 @@ class HighScoresActivity : AppCompatActivity() {
         TabLayoutMediator(
             binding.tabs, binding.pager
         ) { tab, position -> when(position){
-            0 -> tab.text = "Single Player"
-            1 -> tab.text = "Multi Player"
+            0 -> tab.text = getString(R.string.single)
+            1 -> tab.text = getString(R.string.multi)
         } }.attach()
-    }
-
-    override fun onStart() {
-        super.onStart()
-//        scope.launch {
-//            val job = launch {
-//                highscores = ArrayList(FirestoreUtils.highscoresMultiPlayer())
-//            }
-//            if(job.isActive){
-//                runOnUiThread {
-//                    loadingDialog.show()
-//                }
-//            }
-//        }.invokeOnCompletion {
-//            runOnUiThread {
-//                var highScoresList = findViewById<RecyclerView>(R.id.highScoresList)
-//                highScoresList.layoutManager = LinearLayoutManager(
-//                    this@HighScoresActivity,
-//                    LinearLayoutManager.VERTICAL,
-//                    false
-//                )
-//                highScoresList.adapter = HighScoresListAdapter(highscores)
-//                loadingDialog.dismiss()
-//            }
-//        }
-    }
-
-//    class HighScoresListAdapter(val data: ArrayList<SinglePlayerGame>) : RecyclerView.Adapter<HighScoresListAdapter.MyViewHolder>(){
-//        class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
-//            var username : TextView = view.findViewById(R.id.username)
-//            var score : TextView = view.findViewById(R.id.score)
-//            var totalTime : TextView = view.findViewById(R.id.totalTime)
-//
-//            fun update(data : SinglePlayerGame) {
-//                username.text = data.username
-//                score.text = data.score.toString()
-//                totalTime.text = data.time.toString()
-//            }
-//        }
-//
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-//            val view = when(viewType) {
-//                1 -> LayoutInflater.from(parent.context).inflate(R.layout.highscore_list_1,parent,false)
-//                else -> LayoutInflater.from(parent.context).inflate(R.layout.highscore_list_2,parent,false)
-//
-//            }
-//            return MyViewHolder(view)
-//        }
-//
-//        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//            holder.update(data[position])
-//        }
-//
-//        override fun getItemCount(): Int = data.size
-//
-//        override fun getItemViewType(position: Int): Int = when(val boolean = false) {
-//            position % 2 == 0 -> 1
-//            else -> 0
-//        }
-//    }
-
-    private val loadingDialog: AlertDialog by lazy {
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.loading_title))
-            .setMessage(getString(R.string.loading_message))
-            .setView(ProgressBar(this))
-            .create()
     }
 
     class ViewPagerAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
